85a8763673cc367288606b4a998b3ccf84eaef11
Nguyen Trung Kien <nguyenkien11202000@gmail.com>
2023-07-18 02:20:03 +0700

    Setup Project

---
+++ tailwind.config.js
@@ -0,0 +1,10 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  // Chọn tất cả các tệp tin có định dạng .html và .js trong thư mục public và tất cả các thư mục con của nó
+  content: ["./public/**/*.{html,js}"],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+}
+
